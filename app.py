6307c0ae790dc8c80341d527b8b9885edb92d58f
albertissac12 <abhiodf9@gmail.com>
2026-02-22 16:04:47 +0530

    First commit new branch

--- app.py
+++ app.py
@@ -5,28 +5,5 @@ app =  Flask(__name__)
 def index():
     return "<h1>Hello World</h1>"
 
-@app.route("/hello",methods=["POST","GET"])
-def hello():
-    return "<h1>Hello There</h1>"
-
-
-@app.route("/greet/<name>")
-def greet(name):
-    return f"Hello {name}"
-
-@app.route("/add/<int:number1>/<int:number2>")
-def add(number1,number2):
-    return f"<h1>{number1} + {number2} = {number1+number2}</h1>"
-
-@app.route("/handle_url_params")
-def handle_params():
-    if "greeting" in request.args.keys() and "name" in request.args.keys():
-        greeting = request.args.get("greeting")
-        name = request.args.get("name")
-        return f"{greeting},{name}"
-    else:
-        return "Some Parameters are missing"
-
-
 if __name__ == "__main__":
     app.run(debug=True,host="127.0.0.1",port=5555)
