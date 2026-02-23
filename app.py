a895717c1fd9650c35e70732a80581fa8ac677c0
albertissac12 <abhiodf9@gmail.com>
2026-02-23 10:55:09 +0530

    Redirets

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template
-app =  Flask(__name__, template_folder="templates",static_folder="satic")
+from flask import Flask, request, render_template, url_for, redirect, url_for
+app =  Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/")
 def index():
@@ -8,18 +8,26 @@ def index():
     mylist = [x for x in range(10)]
     return render_template("index.html",myvalue=myvalue,myresults=myresults,mylist=mylist)
 
-@app.route("/other")
+@app.route("/other_but_not_other")
 def other():
     some_text = "Some Text"
     return render_template("other.html",some_text=some_text)
 
-@app.template_filter("reveres_string")
-def reveres_string(s):
+
+@app.route("/redirect_endpoint")
+def redirect_endpoint():
+    return redirect(url_for("other"))
+
+
+# Filters
+
+@app.template_filter("reverse_string")
+def reverse_string(s):
     return s[::-1]
 
 @app.template_filter("repeat")
-def repeat(s,times=2):
-    return s * 5
+def repeat(s, times=2):
+    return s * times
 
 @app.template_filter("alternate_case")
 def alternate_case(s):
